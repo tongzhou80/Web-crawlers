8b6f36b52a22c119750963738b583dd9099af210
GentlyGuitar <zt9465@gmail.com>
2014-05-31 12:34:11 +0800

    Update zhihu_crawler.py

--- zhihu_crawler.py
+++ zhihu_crawler.py
@@ -12,8 +12,8 @@ opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 urllib2.install_opener(opener)
 headers ={'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.114 Safari/537.36'}
 url = 'http://www.zhihu.com/login'
-data={"email":"332012407@qq.com",
-      'password':'zt52287800616'}
+data={"email":your email,
+      'password':your password}
 post_data=urllib.urlencode(data)
 req=urllib2.Request(url,post_data,headers)
 HomePage = urllib2.urlopen(req).read()
